5ede282c16a17432bab421bd2950a10eb3524e1b
Emerson Prado <emerson.prado.eng@gmail.com>
2022-09-10 18:33:48 -0300

    Add unit tests for empty class and defined type

---
+++ rspec_test/spec/classes/init_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe 'rspec_test' do
+
+  it { is_expected.to compile }
+
+end
---
+++ rspec_test/spec/defines/rspec_test__type_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe 'rspec_test::type' do
+
+  let(:title) { 'title' }
+
+  it { is_expected.to compile }
+
+end
