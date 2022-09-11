499530133921ba7bcecdfb885bd7e54caaf0ecee
Emerson Prado <emerson.prado.eng@gmail.com>
2022-09-11 16:40:12 -0300

    Test creation of class and defined type

--- rspec_test/spec/classes/init_spec.rb
+++ rspec_test/spec/classes/init_spec.rb
@@ -3,5 +3,6 @@ require 'spec_helper'
 describe 'rspec_test' do
 
   it { is_expected.to compile }
+  it { is_expected.to create_class('rspec_test')}
 
 end
--- rspec_test/spec/defines/rspec_test__type_spec.rb
+++ rspec_test/spec/defines/rspec_test__type_spec.rb
@@ -5,5 +5,6 @@ describe 'rspec_test::type' do
   let(:title) { 'title' }
 
   it { is_expected.to compile }
+  it { is_expected.to create_rspec_test__type('title') }
 
 end
