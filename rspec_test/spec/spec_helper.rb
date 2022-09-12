962d9847a3681c1a5d21cf7bf3929ee2e3cde047
Emerson Prado <emerson.prado.eng@gmail.com>
2022-09-12 15:18:19 -0300

    Fixes warning "defaults `mock_with` to `:mocha`"

--- rspec_test/spec/spec_helper.rb
+++ rspec_test/spec/spec_helper.rb
@@ -1,1 +1,7 @@
+# Fixes warning "defaults `mock_with` to `:mocha`"
+# https://github.com/puppetlabs/puppetlabs_spec_helper#mock_with
+RSpec.configure do |c|
+  c.mock_with :rspec
+end
+
 require 'puppetlabs_spec_helper/module_spec_helper'
