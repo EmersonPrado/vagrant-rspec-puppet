b13dfaa02086d9eb6e4d3ec745558b54c3c483dd
Emerson Prado <emerson.prado.eng@gmail.com>
2022-09-12 15:45:39 -0300

    Add parameter "ensure" in class "rspec_test"

--- rspec_test/manifests/init.pp
+++ rspec_test/manifests/init.pp
@@ -1,7 +1,9 @@
 # Class for unit test learning with rspec-puppet
 class rspec_test (
-  String $type_title,
+  Variant[Boolean, Enum['present', 'absent']] $ensure,
+  Optional[String] $type_title = undef,
 ) {
+  if $ensure == true or $ensure == 'present' {
     rspec_test::type { $type_title: }
   }
 }
--- rspec_test/spec/classes/init_spec.rb
+++ rspec_test/spec/classes/init_spec.rb
@@ -2,12 +2,30 @@ require 'spec_helper'
 
 describe 'rspec_test' do
 
-  let(:params) do {
-    'type_title' => 'any_title',
-  } end
+  context 'with ensure => absent' do
 
-  it { is_expected.to compile }
-  it { is_expected.to create_class('rspec_test')}
-  it { is_expected.to contain_rspec_test__type('any_title') }
+    let(:params) do {
+      'ensure'     => 'absent',
+      'type_title' => 'any_title',
+    } end
+
+    it { is_expected.to compile }
+    it { is_expected.to create_class('rspec_test')}
+    it { is_expected.not_to contain_rspec_test__type('any_title') }
+
+  end
+
+  context 'with ensure => present' do
+
+    let(:params) do {
+      'ensure'     => 'present',
+      'type_title' => 'any_title',
+    } end
+
+    it { is_expected.to compile }
+    it { is_expected.to create_class('rspec_test')}
+    it { is_expected.to contain_rspec_test__type('any_title') }
+
+  end
 
 end
