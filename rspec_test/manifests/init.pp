9802b74b181b6a8aecb13bd5a01b9c8213c129ab
Emerson Prado <emerson.prado.eng@gmail.com>
2022-09-12 15:43:32 -0300

    Add resource "type" in class "rspec_test"

--- rspec_test/manifests/init.pp
+++ rspec_test/manifests/init.pp
@@ -1,2 +1,7 @@
 # Class for unit test learning with rspec-puppet
-class rspec_test {}
+class rspec_test (
+  String $type_title,
+) {
+    rspec_test::type { $type_title: }
+  }
+}
--- rspec_test/spec/classes/init_spec.rb
+++ rspec_test/spec/classes/init_spec.rb
@@ -2,7 +2,12 @@ require 'spec_helper'
 
 describe 'rspec_test' do
 
+  let(:params) do {
+    'type_title' => 'any_title',
+  } end
+
   it { is_expected.to compile }
   it { is_expected.to create_class('rspec_test')}
+  it { is_expected.to contain_rspec_test__type('any_title') }
 
 end
