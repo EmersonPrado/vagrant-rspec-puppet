9a00102ca32ab5457f5305da6b967406e287d57f
Emerson Prado <emerson.prado.eng@gmail.com>
2022-09-12 17:20:58 -0300

    Add OpenSUSE VM

--- bin/post.sh
+++ bin/post.sh
@@ -8,6 +8,13 @@ case "$(cat /proc/version)" in
   *'Red Hat'*)
     dnf -y install ruby rubygem-bundler
   ;;
+  *'SUSE'*)
+    zypper refresh
+    zypper install -y ruby $(
+      zypper search /^ruby[[:digit:]].*-rubygem-bundler$/ |
+      awk '/rubygem-bundler/ { print $2 }'
+    )
+  ;;
   *)
     echo 'Unsupported distro' >&2
     exit 1
